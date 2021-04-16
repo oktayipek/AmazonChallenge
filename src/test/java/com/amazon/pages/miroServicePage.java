3a597dbb6ed03efa85f2bb59eec6eb458a5cf23f
oktayipek <oktay-ipek@hotmail.com>
2021-04-16 13:56:28 +0200

    deleted miro function

--- src/test/java/com/amazon/pages/miroServicePage.java
+++
@@ -1,6 +0,0 @@
-package com.amazon.pages;
-
-public class miroServicePage extends miroBase {
-
-
-}
--- src/test/java/com/amazon/runners/CukesRunner.java
+++ src/test/java/com/amazon/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/amazon/step_definitions",
         dryRun = true,
-        tags = "@miroLogin"
+        tags = "@Login"
 )
 public class CukesRunner {
 }
